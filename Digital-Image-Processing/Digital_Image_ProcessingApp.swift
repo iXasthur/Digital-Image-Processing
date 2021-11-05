702732c44526cac94abf81302973956a2702a490
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-11-05 19:06:59 +0300

    Initial Commit

---
+++ Digital-Image-Processing/Digital_Image_ProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_Image_ProcessingApp.swift
+//  Digital-Image-Processing
+//
+//  Created by Михаил Ковалевский on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Digital_Image_ProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
